d80114fe8f3152ab7606f4433859cecd6c0863b5
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2020-09-05 16:57:35 +0200

    Prepare sql scripts folder. Make use of dotenv

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
