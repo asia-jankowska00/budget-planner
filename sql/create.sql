e7e441006f5cf153c3a8295c5b9388e9d6d4e548
NikolayR21 <1081601@ucn.dk>
2020-09-09 14:18:21 +0200

    create, drop
    Ready for review

--- sql/create.sql
+++ sql/create.sql
@@ -1,32 +1,5 @@
 USE "1081601";
 
--- creating user table
-CREATE TABLE bpUser( 
-    ID INT IDENTITY PRIMARY KEY,
-    firstName NVARCHAR (255) NOT NULL,
-    lastName NVARCHAR (255) NOT NULL,
-    IsDisabled BIT
-);
-
-CREATE TABLE bpCreds(
-    ID INT IDENTITY PRIMARY KEY,
-    userUsername NVARCHAR (255) NOT NULL,
-    userPassword NVARCHAR (255) NOT NULL
-)
-
-CREATE TABLE bpUserCreds(
-    FK_bpUser INT,
-    FK_bpCreds INT,
-
-    CONSTRAINT PK_bpUserCreds PRIMARY KEY(
-        FK_bpUser,
-        FK_bpCreds
-    ),
-
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID),
-    FOREIGN KEY(FK_bpCreds) REFERENCES bpCreds(ID),
-);
-
 -- createing currency table
 CREATE TABLE bpCurrency( 
     ID INT IDENTITY PRIMARY KEY,
@@ -34,18 +7,26 @@ CREATE TABLE bpCurrency(
     currencyCode NVARCHAR (50)
 );
 
--- creating user preffered currency table
-CREATE TABLE bpUserCurrency( 
-    FK_bpUser INT,
+-- creating user table
+CREATE TABLE bpUser( 
+    ID INT IDENTITY PRIMARY KEY,
+    firstName NVARCHAR (255) NOT NULL,
+    lastName NVARCHAR (255) NOT NULL,
+    IsDisabled BIT,
+
     FK_bpCurrency INT,
 
-    CONSTRAINT PK_bpUserCurrency PRIMARY KEY(
-        FK_bpUser,
-        FK_bpCurrency
-    ),
+    FOREIGN KEY (FK_bpCurrency) REFERENCES bpCurrency(ID)
+);
 
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID),
-    FOREIGN KEY(FK_bpCurrency) REFERENCES bpCurrency(ID)
+CREATE TABLE bpLogin(
+    ID INT IDENTITY PRIMARY KEY,
+    userUsername NVARCHAR (255) NOT NULL,
+    userPassword NVARCHAR (255) NOT NULL,
+
+    FK_User INT,
+
+    CONSTRAINT FK_Login_User FOREIGN KEY(FK_User) REFERENCES bpUser(ID)
 );
 
 -- creating source table
@@ -56,7 +37,7 @@ CREATE TABLE bpSource(
     sourceName NVARCHAR (255) NOT NULL,
     sourceDescription NVARCHAR(255),
     amount MONEY NOT NULL, --come back, money format
-    currency NVARCHAR (50) NOT NULL,
+     -- currency NVARCHAR (50) NOT NULL,
 
     FK_bpUser INT,
     FK_bpCurrency INT
@@ -76,20 +57,6 @@ CREATE TABLE bpUserSource(
     ),
 
     FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID),
-    FOREIGN KEY(FK_bpSource) REFERENCES bpSource(ID)
-);
-
--- creating source will have currency table
-CREATE TABLE bpCurrencySource(
-    FK_bpCurrency INT,
-    FK_bpSource INT,
-
-    CONSTRAINT PK_currency_source PRIMARY KEY(
-        FK_bpCurrency,
-        FK_bpSource
-    ),
-
-    FOREIGN KEY(FK_bpCurrency) REFERENCES bpCurrency(ID),
     FOREIGN KEY(FK_bpSource) REFERENCES bpSource(ID)
 );
 
@@ -187,20 +154,6 @@ CREATE TABLE bpContainerTransaction(
     FOREIGN KEY(FK_bpTransaction) REFERENCES bpTransaction(ID)
 );
 
--- creating transaction will have category
-CREATE TABLE bpTransactionCategory(
-    FK_bpTransaction INT,
-    FK_bpCategory INT,
-
-    CONSTRAINT PK_bpTransactionCategory PRIMARY KEY(
-        FK_bpTransaction,
-        FK_bpCategory
-    ),
-
-    FOREIGN KEY(FK_bpTransaction) REFERENCES bpTransaction(ID),
-    FOREIGN KEY(FK_bpCategory) REFERENCES bpCategory(ID)
-);
-
 -- creating notification table
 CREATE TABLE bpNotification(
     ID INT IDENTITY PRIMARY KEY,
@@ -208,20 +161,6 @@ CREATE TABLE bpNotification(
     FK_bpUser INT,
 
     FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID)
-);
-
---creating notification is about transaction
-CREATE TABLE bpTransactionNotification(
-    FK_bpTransaction INT,
-    FK_bpNotification INT,
-
-    CONSTRAINT PK_bpTransactionNotification PRIMARY KEY(
-        FK_bpTransaction,
-        FK_bpNotification
-    ),
-
-    FOREIGN KEY(FK_bpTransaction) REFERENCES bpTransaction(ID),
-    FOREIGN KEY(FK_bpNotification) REFERENCES bpNotification(ID)
 );
 
 --create goal table
@@ -236,46 +175,3 @@ CREATE TABLE bpGoal(
 
     FOREIGN KEY(FK_bpContainer) REFERENCES bpContainer(ID)
 );
-
--- creating goal belongs to container
-CREATE TABLE bpContainerGoal(
-    FK_bpContainer INT,
-    FK_bpGoal INT,
-
-    CONSTRAINT PK_bpContainerGoal PRIMARY KEY(
-        FK_bpContainer,
-        FK_bpGoal
-    ),
-
-    FOREIGN KEY(FK_bpContainer) REFERENCES bpContainer(ID),
-    FOREIGN KEY(FK_bpGoal) REFERENCES bpGoal(ID)
-);
-
-INSERT INTO bpUser(firstName,lastName,IsDisabled)
-VALUES  (@firstName, @lastName, 0);
---#########
-INSERT INTO bpUser(firstName,lastName,IsDisabled)
-VALUES  ('Alexandru', 'Bogdan', 0),
-        ('Joanna', 'Jankowska', 0),
-        ('Nikolay', 'Rusev', 0);
-
-INSERT INTO bpCreds(userUsername,userPassword)
-VALUES  (@userUsername, @userPassword);
--- ########
-INSERT INTO bpCreds(userUsername,userPassword)
-VALUES  ('afbogdan', 'Password123'),
-        ('joanna-00', 'Password456'),
-        ('nikolayr21', 'Password789');
-
-INSERT INTO bpCurrency(currencyName,currencyCode)
-VALUES  (@currencyName, @currencyCode);
--- ########
-INSERT INTO bpCurrency(currencyName,currencyCode)
-VALUES  ('Danish Krone', 'DKK'),
-        ('Euro', 'EUR'),
-        ('Bulgarian Lev', 'BGN'),
-        ('Zloty', 'PLN'),
-        ('Romanian Leu', 'RON');
-
-INSERT INTO bpSource(sourceName, sourceDescription, amount, currency)
-VALUES  ()
