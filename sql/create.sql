aa72c64eff6c44e63b051aa068b9c67a12cf2415
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-09-10 12:54:38 +0200

    Rework DB namespaces

--- sql/create.sql
+++ sql/create.sql
@@ -2,176 +2,177 @@ USE "1081601";
 
 -- createing currency table
 CREATE TABLE bpCurrency( 
-    ID INT IDENTITY PRIMARY KEY,
-    currencyName NVARCHAR (255),
-    currencyCode NVARCHAR (50)
+    CurrencyId INT IDENTITY PRIMARY KEY,
+    CurrencyName NVARCHAR (255),
+    CurrencyCode NVARCHAR (50)
 );
 
 -- creating user table
 CREATE TABLE bpUser( 
-    ID INT IDENTITY PRIMARY KEY,
-    firstName NVARCHAR (255) NOT NULL,
-    lastName NVARCHAR (255) NOT NULL,
-    isDisabled BIT,
+    UserId INT IDENTITY PRIMARY KEY,
+    UserFirstName NVARCHAR (255) NOT NULL,
+    UserLastName NVARCHAR (255) NOT NULL,
+    UserIsDisabled BIT,
 
-    FK_bpCurrency INT,
+    CurrencyId INT,
 
-    FOREIGN KEY (FK_bpCurrency) REFERENCES bpCurrency(ID)
+    FOREIGN KEY (CurrencyId) REFERENCES bpCurrency(CurrencyId)
 );
 
 CREATE TABLE bpLogin(
-    ID INT IDENTITY PRIMARY KEY,
-    userUsername NVARCHAR (255) NOT NULL,
-    userPassword NVARCHAR (255) NOT NULL,
+    LoginId INT IDENTITY PRIMARY KEY,
+    LoginUsername NVARCHAR (255) NOT NULL,
+    LoginPassword NVARCHAR (255) NOT NULL,
 
-    FK_User INT,
+    UserId INT,
 
-    CONSTRAINT FK_Login_User FOREIGN KEY(FK_User) REFERENCES bpUser(ID)
+    CONSTRAINT LoginUser FOREIGN KEY(UserId) REFERENCES bpUser(UserId)
 );
 
 -- creating source table
 -- user owns source
 -- currency can be used in source
 CREATE TABLE bpSource(
-    ID INT IDENTITY PRIMARY KEY,
-    sourceName NVARCHAR (255) NOT NULL,
-    sourceDescription NVARCHAR(255),
-    amount MONEY NOT NULL, --come back, money format
+    SourceId INT IDENTITY PRIMARY KEY,
+    SourceName NVARCHAR (255) NOT NULL,
+    SourceDescription NVARCHAR(255),
+    SourceAmount MONEY NOT NULL, --come back, money format
      -- currency NVARCHAR (50) NOT NULL,
 
-    FK_bpUser INT,
-    FK_bpCurrency INT
+    UserId INT,
+    CurrencyId INT
 
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID),
-    FOREIGN KEY(FK_bpCurrency) REFERENCES bpCurrency(ID)
+    FOREIGN KEY(UserId) REFERENCES bpUser(UserId),
+    FOREIGN KEY(CurrencyId) REFERENCES bpCurrency(CurrencyId)
 );
 
 -- creating user can use source table
 CREATE TABLE bpUserSource(
-    FK_bpUser INT,
-    FK_bpSource INT,
+    UserId INT,
+    SourceId INT,
 
     CONSTRAINT PK_bpUserSource PRIMARY KEY(
-        FK_bpUser,
-        FK_bpSource
+        UserId,
+        SourceId
     ),
 
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID),
-    FOREIGN KEY(FK_bpSource) REFERENCES bpSource(ID)
+    FOREIGN KEY(UserId) REFERENCES bpUser(UserId),
+    FOREIGN KEY(SourceId) REFERENCES bpSource(SourceId)
 );
 
 --creating container table
 -- user owns container
 CREATE TABLE bpContainer(
-    ID INT IDENTITY PRIMARY KEY,
-    containerName NVARCHAR (255) NOT NULL,
+    ContainerId INT IDENTITY PRIMARY KEY,
+    ContainerName NVARCHAR (255) NOT NULL,
 
-    FK_bpUser INT,
+    UserId INT,
 
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID)
+    FOREIGN KEY(UserId) REFERENCES bpUser(UserId)
 );
 
 -- creating user has access to container
 CREATE TABLE bpUserContainer(
-    FK_bpUser INT,
-    FK_bpContainer INT,
+    UserId INT,
+    ContainerId INT,
 
-    CONSTRAINT PK_bpUserContainer PRIMARY KEY(
-        FK_bpUser,
-        FK_bpContainer
+    CONSTRAINT UserContainer PRIMARY KEY(
+        UserId,
+        ContainerId
     ),
 
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID),
-    FOREIGN KEY(FK_bpContainer) REFERENCES bpContainer(ID)
+    FOREIGN KEY(UserId) REFERENCES bpUser(ID),
+    FOREIGN KEY(ContainerId) REFERENCES bpContainer(ID)
 );
 
 -- creating container includes source table
 CREATE TABLE bpContainerSource(
-    FK_bpContainer INT,
-    FK_bpSource INT,
+    ContainerId INT,
+    SourceId INT,
 
-    CONSTRAINT PK_bpContainerSource PRIMARY KEY(
-        FK_bpContainer,
-        FK_bpSource
+    CONSTRAINT ContainerSource PRIMARY KEY(
+        ContainerId,
+        SourceId
     ),
 
-    FOREIGN KEY(FK_bpContainer) REFERENCES bpContainer(ID),
-    FOREIGN KEY(FK_bpSource) REFERENCES bpSource(ID)
+    FOREIGN KEY(ContainerId) REFERENCES bpContainer(ContainerId),
+    FOREIGN KEY(SourceId) REFERENCES bpSource(SourceId)
 );
 
 -- create category table
 -- can be found in
 CREATE TABLE bpCategory(
-    ID INT IDENTITY PRIMARY KEY,
-    categoryName NVARCHAR (255) NOT NULL
+    CategoryId INT IDENTITY PRIMARY KEY,
+    CategoryName NVARCHAR (255) NOT NULL,
 );
 
 -- creating container has category
 CREATE TABLE bpContainerCategory(
-    FK_bpContainer INT,
-    FK_bpCategory INT
+    ContainerId INT,
+    CategoryId INT,
+    CategoryEstimation INT,
 
-    CONSTRAINT PK_bpContainerCategory PRIMARY KEY(
-        FK_bpContainer,
-        FK_bpCategory
+    CONSTRAINT ContainerCategory PRIMARY KEY(
+        ContainerId,
+        CategoryId
     ),
 
-    FOREIGN KEY(FK_bpContainer) REFERENCES bpContainer(ID),
-    FOREIGN KEY(FK_bpCategory) REFERENCES bpCategory(ID)
+    FOREIGN KEY(ContainerId) REFERENCES bpContainer(ContainerId),
+    FOREIGN KEY(CategoryId) REFERENCES bpCategory(CategoryId)
 );
 
 -- creating transaction table
 -- transaction is made with source
 -- user has made a transaction
 CREATE TABLE bpTransaction(
-    ID INT IDENTITY PRIMARY KEY,
-    transactionName NVARCHAR (255) NOT NULL,
-    transactionDate DATETIME,
-    amount MONEY,--data type for money
-    isExpense BIT NOT NULL,
-    note NVARCHAR (255),
+    TransactionId INT IDENTITY PRIMARY KEY,
+    TransactionName NVARCHAR (255) NOT NULL,
+    TransactionDate DATETIME,
+    TransactionAmount MONEY,--data type for money
+    TransactionIsExpense BIT NOT NULL,
+    TransactionNote NVARCHAR (255),
 
-    FK_bpUser INT,
-    FK_bpSource INT,
-    FK_bpCategory INT,
+    UserId INT,
+    SourceId INT,
+    CategoryId INT,
 
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID),
-    FOREIGN KEY(FK_bpSource) REFERENCES bpSource(ID),
-    FOREIGN KEY(FK_bpCategory) REFERENCES bpCategory(ID)
+    FOREIGN KEY(UserId) REFERENCES bpUser(UserId),
+    FOREIGN KEY(SourceId) REFERENCES bpSource(SourceId),
+    FOREIGN KEY(CategoryId) REFERENCES bpCategory(CategoryId)
 );
 
 -- creating transaction is visible in container table
 CREATE TABLE bpContainerTransaction(
-    FK_bpContainer INT,
-    FK_bpTransaction INT,
+    ContainerId INT,
+    TransactionId INT,
 
     CONSTRAINT PK_container_appTransaction PRIMARY KEY(
-        FK_bpContainer,
-        FK_bpTransaction
+        ContainerId,
+        TransactionId
     ),
 
-    FOREIGN KEY(FK_bpContainer) REFERENCES bpContainer(ID),
-    FOREIGN KEY(FK_bpTransaction) REFERENCES bpTransaction(ID)
+    FOREIGN KEY(ContainerId) REFERENCES bpContainer(ContainerId),
+    FOREIGN KEY(TransactionId) REFERENCES bpTransaction(TransactionId)
 );
 
 -- creating notification table
 CREATE TABLE bpNotification(
-    ID INT IDENTITY PRIMARY KEY,
+    NotificationId INT IDENTITY PRIMARY KEY,
 
-    FK_bpUser INT,
+    UserId INT,
 
-    FOREIGN KEY(FK_bpUser) REFERENCES bpUser(ID)
+    FOREIGN KEY(UserId) REFERENCES bpUser(UserId)
 );
 
 --create goal table
 --container has a goal
 CREATE TABLE bpGoal(
-    ID INT IDENTITY PRIMARY KEY,
-    goalName NVARCHAR (255) NOT NULL,
-    amount MONEY, 
-    deadline DATE,
+    GoalId INT IDENTITY PRIMARY KEY,
+    GoalName NVARCHAR (255) NOT NULL,
+    GoalAmount MONEY, 
+    GoalDeadline DATE,
 
-    FK_bpContainer INT,
+    ContainerId INT,
 
-    FOREIGN KEY(FK_bpContainer) REFERENCES bpContainer(ID)
+    FOREIGN KEY(ContainerId) REFERENCES bpContainer(ContainerId)
 );
--- sql/testData.sql
+++ sql/testData.sql
@@ -5,7 +5,7 @@ USE "1081601";
 -- INSERT INTO bpCurrency(currencyName,currencyCode)
 -- VALUES  (@currencyName, @currencyCode);
 -- ########
-INSERT INTO bpCurrency(currencyName,currencyCode)
+INSERT INTO bpCurrency(CurrencyName, CurrencyCode)
 VALUES  ('Danish Krone', 'DKK'),
         ('Euro', 'EUR'),
         ('Bulgarian Lev', 'BGN'),
@@ -15,7 +15,7 @@ VALUES  ('Danish Krone', 'DKK'),
 -- INSERT INTO bpUser(firstName,lastName,IsDisabled)
 -- VALUES  (@firstName, @lastName, 0);
 --#########
-INSERT INTO bpUser(firstName,lastName,IsDisabled, FK_bpCurrency)
+INSERT INTO bpUser(UserFirstName, UserLastName, UserIsDisabled, CurrencyId)
 VALUES  ('Alexandru', 'Bogdan', 0, 1),
         ('Joanna', 'Jankowska', 0, 4),
         ('Nikolay', 'Rusev', 0, 2);
@@ -23,17 +23,17 @@ VALUES  ('Alexandru', 'Bogdan', 0, 1),
 -- INSERT INTO bpLogin(userUsername,userPassword)
 -- VALUES  (@userUsername, @userPassword);
 -- ########
-INSERT INTO bpLogin(userUsername,userPassword, FK_User)
+INSERT INTO bpLogin(LoginUsername, LoginPassword, UserId)
 VALUES  ('afbogdan', 'Password123', 1),
         ('joanna-00', 'Password456', 2),
         ('nikolayr21', 'Password789', 3);
 
-INSERT INTO bpSource(sourceName, sourceDescription, amount, FK_bpUser,FK_bpCurrency)
+INSERT INTO bpSource(SourceName, SourceDescription, SourceAmount, UserId, CurrencyId)
 VALUES  ('Sparnord', 'Main bank account', 14000.35, 1, 1),
         ('Cash', 'Emergency on-hand money', 10000, 2, 4),
         ('Savings', 'For trips', 16000, 3, 2);
 
-INSERT INTO bpContainer(containerName, FK_bpUser)
+INSERT INTO bpContainer(ContainerName, UserId)
 VALUES  ('Private budget', 1),
         ('Shared budget', 1),
         ('Private budget', 2),
@@ -41,7 +41,7 @@ VALUES  ('Private budget', 1),
         ('Private budget', 3),
         ('Shared budget', 3);
 
-INSERT INTO bpCategory(categoryName)
+INSERT INTO bpCategory(CategoryName)
 VALUES  ('Groceries'),
         ('Clothes'),
         ('Transport'),
@@ -50,7 +50,7 @@ VALUES  ('Groceries'),
         ('Footwear'),
         ('Bills');
 
-INSERT INTO bpTransaction (transactionName, transactionDate, amount, isExpense, FK_bpUser, FK_bpSource, FK_bpCategory)
+INSERT INTO bpTransaction (TransactionName, TransactionDate, TransactionAmount, TransactionIsExpense, UserId, SourceId, CategoryId)
 VALUES  ('Weekly groceries', 2020-09-25, 250, 1, 2, 2, 1),
         ('6 month DR subscription', 2020-06-01, 660, 1, 1, 1, 7),
         ('1 year Game card', 2019-11-20, 372, 1, 3, 3, 4),
@@ -58,7 +58,7 @@ VALUES  ('Weekly groceries', 2020-09-25, 250, 1, 2, 2, 1),
         ('Spotify subscription',2020-09-10, 40, 1,  3, 3, 4),
         ('Netflix subscription', 2020-08-28, 160, 1, 1, 1, 4);
 
-INSERT INTO bpContainerSource(FK_bpContainer, FK_bpSource) VALUES  
+INSERT INTO bpContainerSource(ContainerId, SourceId) VALUES  
         (1, 1),
         (2, 1),
         (3, 2),
@@ -81,34 +81,34 @@ SELECT * FROM bpCategory;
 SELECT * FROM bpTransaction;
 SELECT * FROM bpContainerSource;
 
-SELECT firstName, lastName, FK_bpCurrency
+SELECT UserFirstName, UserLastName, CurrencyId
 FROM bpUser
 INNER JOIN bpLogin
-ON bpLogin.FK_User = bpUser.ID
-WHERE bpLogin.userUsername = 'afbogdan' AND bpLogin.userPassword = 'Password123' AND bpUser.IsDisabled = 0;
+ON bpLogin.UserId = bpUser.UserId
+WHERE bpLogin.LoginUsername = 'afbogdan' AND bpLogin.LoginPassword = 'Password123' AND bpUser.UserIsDisabled = 0;
 
-SELECT bpUser.ID, bpUser.firstName, bpUser.lastName, bpCurrency.currencyName, bpCurrency.currencyCode
+SELECT bpUser.UserId, bpUser.UserFirstName, bpUser.UserLastName, bpCurrency.CurrencyName, bpCurrency.CurrencyCode
 FROM bpUser
 INNER JOIN bpLogin
-ON bpLogin.FK_User = bpUser.ID
+ON bpLogin.UserId = bpUser.UserId
 INNER JOIN bpCurrency
-ON bpUser.FK_bpCurrency = bpCurrency.ID
-WHERE bpLogin.userUsername = 'afbogdan'
-AND bpLogin.userPassword = 'Password123'
-AND bpUser.IsDisabled = 0
-AND bpCurrency.ID = bpUser.FK_bpCurrency;
+ON bpUser.CurrencyId = bpCurrency.CurrencyId
+WHERE bpLogin.LoginUsername = 'afbogdan'
+AND bpLogin.LoginPassword = 'Password123'
+AND bpUser.UserIsDisabled = 0
+AND bpCurrency.CurrencyId = bpUser.CurrencyId;
 
-SELECT firstName, lastName, sourceName, amount
+SELECT UserFirstName, UserLastName, SourceName, SourceAmount
 FROM bpUser
 INNER JOIN bpSource
-ON bpSource.FK_bpUser = bpUser.ID;
+ON bpSource.UserId = bpUser.UserId;
 
-SELECT firstName, lastName, containerName, bpContainer.ID
+SELECT UserFirstName, UserLastName, ContainerName, bpContainer.ContainerId
 FROM bpUser
 INNER JOIN bpContainer
-ON bpContainer.FK_bpUser = bpUser.ID;
+ON bpContainer.UserId = bpUser.UserId;
 
-SELECT bpContainer.containerName, bpContainer.FK_bpUser, bpContainerSource.FK_bpContainer, bpContainerSource.FK_bpSource
+SELECT bpContainer.ContainerName, bpContainer.UserId, bpContainerSource.ContainerId, bpContainerSource.SourceId
 FROM bpContainer
 INNER JOIN bpContainerSource
-ON bpContainerSource.FK_bpContainer = bpContainer.ID;
+ON bpContainerSource.ContainerId = bpContainer.ContainerId;
