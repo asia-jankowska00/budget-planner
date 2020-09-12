92e9c02610e86d5577443bfdb53799fe6bf15f48
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-09-12 21:11:56 +0200

    Add users schema

--- server/routes/schemas/usersSchema.js
+++ server/routes/schemas/usersSchema.js
@@ -1,3 +1,14 @@
 const Joi = require("joi");
 
+// GET /users&q=queryString
+
+const searchUsersOutput = Joi.array().items(
+  Joi.object({
+    id: Joi.number().integer().min(1).required(),
+    username: Joi.string().min(1).max(255).required(),
+    firstName: Joi.string().min(1).max(255).required(),
+    lastName: Joi.string().min(1).max(255).required(),
+  })
+);
+
 module.exports = {};
