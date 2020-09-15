e2891637dad1b0102c7f4ba868968c79447c0592
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2020-09-15 18:47:20 +0200

    Update user schema and search query

--- server/routes/schemas/userSchemas.js
+++ server/routes/schemas/userSchemas.js
@@ -2,13 +2,16 @@ const Joi = require("joi");
 
 // GET /users&q=queryString
 
-const searchUsersOutput = Joi.array().items(
-  Joi.object({
-    id: Joi.number().integer().min(1).required(),
-    username: Joi.string().min(1).max(255).required(),
-    firstName: Joi.string().min(1).max(255).required(),
-    lastName: Joi.string().min(1).max(255).required(),
-  })
-);
+const defaultUser = Joi.object({
+  id: Joi.number().integer().min(1).required(),
+  username: Joi.string().min(1).max(255).required(),
+  firstName: Joi.string().min(1).max(255).required(),
+  lastName: Joi.string().min(1).max(255).required(),
+}) 
 
-module.exports = { searchUsersOutput };
+const searchUsersOutput = Joi.array().items(defaultUser);
+
+module.exports = { 
+  defaultUser,
+  searchUsersOutput 
+};
