a20e2257a1788102675163b8d7397eabff94fa24
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2020-09-18 10:30:49 +0200

    Prepare transaction model, schemas and router

---
+++ server/routes/schemas/transactionSchemas.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+const { defaultUser } = require('./userSchemas');
+
+const defaultTransaction = {
+  id: Joi.number().integer().min(1).required(),
+  name: Joi.string().min(1).max(255).required(),
+  date: Joi.date(),
+  amount: Joi.number().precision(4).required(),
+  isExpense: Joi.bool().required(),
+  note: Joi.string().min(1).max(255),
+  user: Joi.object(defaultUser)
+}
+
+const postTransactionInput = {
+  name: Joi.string().min(1).max(255).required(),
+  date: Joi.date(),
+  amount: Joi.number().precision(4).required(),
+  isExpense: Joi.bool().required(),
+  note: Joi.string().min(1).max(255),
+  containerId: Joi.number().integer().min(1).required(),
+  sourceId: Joi.number().integer().min(1).required(),
+  categoryId: Joi.number().integer().min(1)
+}
+
+const defaultTransactionOutput = Joi.object(defaultTransaction);
+
+module.exports = {
+  defaultTransaction,
+  postTransactionInput,
+  defaultTransactionOutput
+}
