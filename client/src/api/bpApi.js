c699e98a5a09d4c342ca468dfff2736d401b3e1b
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2020-09-19 12:20:25 +0200

    Fix token bug on client-side

--- client/src/api/bpApi.js
+++ client/src/api/bpApi.js
@@ -2,32 +2,39 @@ import axios from 'axios'
 
 const PORT = 8550;
 const baseUrl = `http://localhost:${PORT}/api`;
-const token = localStorage.getItem('bpToken');
 
 axios.defaults.headers.common['Content-Type'] = 'application/json-patch+json';
-axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+
+const getConfig = () => {
+    const token = localStorage.getItem('bpToken');
+    return {
+        headers: {
+            'Authorization': "Bearer " + token
+        }
+    }
+}
 
 export default {
     auth() {
         return {
-            register: (userObj) => axios.post(`${baseUrl}/auth/register`, userObj),
-            login: (userObj) => axios.post(`${baseUrl}/auth/login`, userObj) 
+            register: (userObj) => axios.post(`${baseUrl}/auth/register`, userObj, getConfig()),
+            login: (userObj) => axios.post(`${baseUrl}/auth/login`, userObj, getConfig()) 
         }
     },
     currencies() {
         return {
-            getAll: () => axios.get(`${baseUrl}/currencies`)
+            getAll: () => axios.get(`${baseUrl}/currencies`, getConfig())
         }
     },
     users() {
         return {
-            getProfile: () => axios.get(`${baseUrl}/profile`),
-            updateProfile: (userObj) => axios.patch(`${baseUrl}/profile`, userObj)
+            getProfile: () => axios.get(`${baseUrl}/profile`, getConfig()),
+            updateProfile: (userObj) => axios.patch(`${baseUrl}/profile`, userObj, getConfig())
         }
     },
     sources() {
         return {
-            add: (source) => axios.post(`${baseUrl}/sources`, source) 
+            add: (source) => axios.post(`${baseUrl}/sources`, source, getConfig()) 
         }
     }
 }
