2cb57538a937bc5ba9611238b01b0cebda0f7fb6
NikolayR21 <1081601@ucn.dk>
2020-09-20 23:00:15 +0200

    all TransactionContatiner

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -91,11 +91,112 @@ class Transaction{
   }
 
   static getAllContainerTransactions(containerId) {
+    return new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          const pool = await sql.connect(connection);
+          const result = await pool.request()
+          .input('ContainerId', sql.Int, containerId)
+          .query(`
+          SELECT
+          bpTransaction.TransactionId,
+          TransactionName,
+          TransactionDate,
+          TransactionAmount,
+          TransactionIsExpense,
+          TransactionNote,
+          bpTransaction.SourceId,
+          bpTransaction.UserId,
+          SourceName,
+          UserFirstName, UserLastName, LoginUsername
+          FROM bpTransaction
+          INNER JOIN bpContainerTransaction
+          ON bpContainerTransaction.TransactionId = bpTransaction.TransactionId
+          INNER JOIN bpSource
+          ON bpSource.SourceId = bpTransaction.SourceId
+          INNER JOIN bpUser
+          ON bpTransaction.UserId = bpUser.UserId
+          INNER JOIN bpLogin
+          ON bpTransaction.UserId = bpLogin.UserId 
+          WHERE bpContainerTransaction.ContainerId = @ContainerId
+          `)
 
+          if (result.recordset < 0)
+          throw {status: 404 , message: 'No transactions found.'}
+
+          const transactions = [];
+          if(result.recordset.length > 1){
+            result.recordset.forEach((record) => {
+            const transactionObj = {
+              id: record.TransactionId,
+              name: record.TransactionName,
+              amount: record.TransactionAmount,
+              isExpense: record.TransactionIsExpense,
+              date: record.TransactionDate,
+              note: record.TransactionNote,
+              user: {
+                id: record.UserId,
+                username: record.LoginUsername,
+                firstName: record.UserFirstName,
+                lastName: record.UserLastName
+              }
+            }
+
+            transactions.push(new Transaction(transactionObj))
+          })
+          }
+          
+
+          resolve(transactions);
+        } catch (err) {
+          console.log(err);
+          reject(err);
+        }
+        sql.close();
+      })()
+    })
   }
 
   static getContainerTransaction(containerId, transactionId) {
-    
+    return new Promise ((resolve, reject) => {
+      (async ()=>{
+        try {
+          const pool = await sql.connect(connection);
+          const result = await pool.request()
+          .input('ContainerId', sql.Int, containerId)
+          .input('TransactionId', sql.Int, transactionId)
+          .query(`
+          
+          `)
+
+          if (!result.recordset[0]) { throw {status: 500, message: "Failed to get category"}}
+
+          const record = result.recordset[0];
+          const transaction = new Transaction({
+              id: record.TransactionId,
+              name: record.TransactionName,
+              amount: record.TransactionAmount,
+              isExpense: record.TransactionIsExpense,
+              date: record.TransactionDate,
+              note: record.TransactionNote,
+              user: {
+                id: record.UserId,
+                username: record.LoginUsername,
+                firstName: record.UserFirstName,
+                lastName: record.UserLastName
+              }
+          })
+
+          resolve(transaction)
+
+        } catch (err) {
+          console.log(err);
+          reject(err);
+        }
+
+      sql.close();
+      })()
+    })
   }
 }
 
