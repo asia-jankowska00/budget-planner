d67976f7a978ebb269a8ec2f5decd2a07cc89e48
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-09-21 16:56:16 +0200

    Add category checkOwner

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,71 +1,83 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const Category = require('../models/category');
-const categorySchemas = require('./schemas/categorySchemas');
+const Category = require("../models/category");
+const Container = require("../models/container");
+const categorySchemas = require("./schemas/categorySchemas");
 
-router.post('/', async (req, res) => {
-    try {
-        
-        await categorySchemas.postInputCategory.validateAsync(req.body);
-        
-        const newCategory = await Category.create(req.params.containerId, req.body);
+router.post("/", async (req, res) => {
+  try {
+    await categorySchemas.postInputCategory.validateAsync(req.body);
 
-        await categorySchemas.defaultOutputCategory.validateAsync(newCategory);
-        res.status(201).json(newCategory)
-    } catch (err){
-        console.log(err)
-        res.status(err.status || 400).json(err);
-    }
-})
+    await Container.checkOwner(req.params.containerId, req.user.id);
 
-router.get('/', async (req, res) => {
-    try {
-        const categories = await Category.readAll(req.params.containerId);
+    const newCategory = await Category.create(req.params.containerId, req.body);
 
-        await categorySchemas.getCategoriesOutput.validateAsync(categories);
-        res.json(categories);
-    } catch (err){
-        console.log(err)
-        res.status(err.status || 400).json(err);
-    }
-})
+    await categorySchemas.defaultOutputCategory.validateAsync(newCategory);
+    res.status(201).json(newCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(err.status || 400).json(err);
+  }
+});
 
-router.get('/:categoryId', async (req, res) => {
-    try {
-        const category = await Category.readById(req.params.containerId, req.params.categoryId);
+router.get("/", async (req, res) => {
+  try {
+    const categories = await Category.readAll(req.params.containerId);
 
-        await categorySchemas.defaultOutputCategory.validateAsync(category);
+    await categorySchemas.getCategoriesOutput.validateAsync(categories);
+    res.json(categories);
+  } catch (err) {
+    console.log(err);
+    res.status(err.status || 400).json(err);
+  }
+});
 
-        res.json(category);
-    } catch (err){
-        console.log(err)
-        res.status(err.status || 400).json(err);
-    }
-})
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const category = await Category.readById(
+      req.params.containerId,
+      req.params.categoryId
+    );
 
-router.patch('/:categoryId', async (req, res) => {
-    try {
-        await categorySchemas.postInputCategory.validateAsync(req.body);
+    await categorySchemas.defaultOutputCategory.validateAsync(category);
 
-        const category = await Category.readById(req.params.containerId, req.params.categoryId);
-        await category.update(req.body);
+    res.json(category);
+  } catch (err) {
+    console.log(err);
+    res.status(err.status || 400).json(err);
+  }
+});
 
-        await categorySchemas.defaultOutputCategory.validateAsync(category);
-        res.json(category)
-    } catch (err){
-        console.log(err)
-        res.status(err.status || 400).json(err);
-    }
-})
+router.patch("/:categoryId", async (req, res) => {
+  try {
+    await categorySchemas.postInputCategory.validateAsync(req.body);
 
-router.delete('/:categoryId', async (req, res) => {
-    try {
-        await Category.delete(req.params.containerId, req.params.categoryId);
-        res.json('Category deleted')
-    } catch (err){
-        console.log(err)
-        res.status(err.status || 400).json(err);
-    }
-})
+    await Container.checkOwner(req.params.containerId, req.user.id);
+
+    const category = await Category.readById(
+      req.params.containerId,
+      req.params.categoryId
+    );
+    await category.update(req.body);
+
+    await categorySchemas.defaultOutputCategory.validateAsync(category);
+    res.json(category);
+  } catch (err) {
+    console.log(err);
+    res.status(err.status || 400).json(err);
+  }
+});
+
+router.delete("/:categoryId", async (req, res) => {
+  try {
+    await Container.checkOwner(req.params.containerId, req.user.id);
+
+    await Category.delete(req.params.containerId, req.params.categoryId);
+    res.json("Category deleted");
+  } catch (err) {
+    console.log(err);
+    res.status(err.status || 400).json(err);
+  }
+});
 
 module.exports = router;
