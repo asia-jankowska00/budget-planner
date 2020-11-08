c8bb3aa290f182a930d5d9bfe5f4b95bc69b4b34
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-11-08 09:37:14 +0100

    Move pool config to env

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    port: process.env.PORT,
-  });
+const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
