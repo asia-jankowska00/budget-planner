ae6d8e29f3d43441f634964f107d2039c75bd546
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-01-13 12:42:49 +0100

    switch to hash mode

--- client/deploy.sh
+++ client/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f origin master:gh-pages
+git push -f https://asia-jankowska00.github.io/budget-planner master:gh-pages
 
 cd -
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -76,7 +76,7 @@ const routes = [
 
 const router = new VueRouter({
   base: "/budget-planner",
-  mode: "history",
+  mode: "hash",
   routes,
 });
 
